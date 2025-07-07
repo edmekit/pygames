cbc4c88ed29abf62c947a634be6a5606b576c9b5
Edmarc Mekitpekit <mekitpekit.eggmarc@gmail.com>
2025-07-07 23:41:47 +0800

    put picture

--- first.py
+++ first.py
@@ -1,5 +1,38 @@
 import pygame 
-
 pygame.init()
+width = 800
+height = 600
 
-screen = pygame.display.set_mode((500, 500))
+black = (0, 0, 0)
+white = (255, 255, 255)
+
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("First Game")
+clock = pygame.time.Clock()
+
+img = pygame.image.load("ed.png")
+def draw(x, y):
+    screen.fill(white)
+    screen.blit(img, (x, y))
+                
+x = (width * .45)
+y = (height * .5)
+
+draw(x, y)
+
+crash = False
+
+while not crash:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            crash = True
+    
+    screen.fill(white)
+    draw(x, y)
+
+    
+    pygame.display.update()
+    clock.tick(60)
+
+pygame.quit()
+quit()
